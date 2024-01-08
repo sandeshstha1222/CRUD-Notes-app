ce1d974988721eec36054fcecbc316d7539efb64
sandeshstha1222 <sananddesh1029@gmail.com>
2024-01-08 14:14:24 +0545

    animation for adding added

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaCirclePlus } from "react-icons/fa6";
 import "./Sidebar.css";
 
 const Sidebar = () => {
   const color = ["#fe9b72", "#fec971", " #00d4fe", "#b693fd", "#e4ee91"];
+  const [listOpen, setListOpen] = useState(false);
   return (
     <div className="sidebar">
-      <FaCirclePlus size={50} alt="Add" className="sidebar_add-icon" />
-      <ul className="sidebar_color-list">
+      <FaCirclePlus
+        size={50}
+        alt="Add"
+        className="sidebar_add-icon"
+        onClick={() => setListOpen(!listOpen)}
+      />
+      <ul
+        className={`sidebar_color-list ${
+          listOpen ? "sidebar_color-list_active" : ""
+        }`}
+      >
         {color.map((item, index) => (
           <li
             key={index}
