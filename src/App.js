a023ef6eba78a3dd1e47fae38d14a15fa0697393
sandeshstha1222 <sananddesh1029@gmail.com>
2024-01-08 14:46:06 +0545

    dynamic note

--- src/App.js
+++ src/App.js
@@ -4,10 +4,42 @@ import NoteContainer from "./components/NoteContainer/NoteContainer";
 import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
+  const notes = [
+    {
+      text: "sad",
+      time: "1:50pm",
+      color: "orange",
+    },
+    {
+      text: "sad",
+      time: "1:50pm",
+      color: "orange",
+    },
+    {
+      text: "sad",
+      time: "1:50pm",
+      color: "orange",
+    },
+    {
+      text: "sad",
+      time: "1:50pm",
+      color: "orange",
+    },
+    {
+      text: "sad",
+      time: "1:50pm",
+      color: "orange",
+    },
+    {
+      text: "sad",
+      time: "1:50pm",
+      color: "orange",
+    },
+  ];
   return (
     <div className="App">
       <Sidebar />
-      <NoteContainer />
+      <NoteContainer notes={notes} />
     </div>
   );
 }
--- src/components/NoteContainer/NoteContainer.js
+++ src/components/NoteContainer/NoteContainer.js
@@ -2,137 +2,14 @@ import React from "react";
 import "./NoteContainer.css";
 import Note from "../Note/Note";
 
-const NoteContainer = () => {
+const NoteContainer = (props) => {
   return (
     <div className="note-container">
       <h2>Notes</h2>
       <div className="note-container_notes custom-scroll">
-        <Note
-          note={{
-            text: "sad",
-            time: "1:00PM",
-            color: "cyan",
-          }}
-        />
-        <Note
-          note={{
-            text: "sad",
-            time: "1:00PM",
-            color: "cyan",
-          }}
-        />
-        <Note
-          note={{
-            text: "sad",
-            time: "1:00PM",
-            color: "cyan",
-          }}
-        />
-        <Note
-          note={{
-            text: "sad",
-            time: "1:00PM",
-            color: "cyan",
-          }}
-        />
-        <Note
-          note={{
-            text: "sad",
-            time: "1:00PM",
-            color: "cyan",
-          }}
-        />
-        <Note
-          note={{
-            text: "sad",
-            time: "1:00PM",
-            color: "cyan",
-          }}
-        />
-        <Note
-          note={{
-            text: "sad",
-            time: "1:00PM",
-            color: "cyan",
-          }}
-        />
-        <Note
-          note={{
-            text: "sad",
-            time: "1:00PM",
-            color: "cyan",
-          }}
-        />
-        <Note
-          note={{
-            text: "sad",
-            time: "1:00PM",
-            color: "cyan",
-          }}
-        />
-        <Note
-          note={{
-            text: "sad",
-            time: "1:00PM",
-            color: "cyan",
-          }}
-        />
-        <Note
-          note={{
-            text: "sad",
-            time: "1:00PM",
-            color: "cyan",
-          }}
-        />
-        <Note
-          note={{
-            text: "sad",
-            time: "1:00PM",
-            color: "cyan",
-          }}
-        />
-        <Note
-          note={{
-            text: "sad",
-            time: "1:00PM",
-            color: "cyan",
-          }}
-        />
-        <Note
-          note={{
-            text: "sad",
-            time: "1:00PM",
-            color: "cyan",
-          }}
-        />
-        <Note
-          note={{
-            text: "sad",
-            time: "1:00PM",
-            color: "cyan",
-          }}
-        />
-        <Note
-          note={{
-            text: "sad",
-            time: "1:00PM",
-            color: "cyan",
-          }}
-        />
-        <Note
-          note={{
-            text: "sad",
-            time: "1:00PM",
-            color: "cyan",
-          }}
-        />
-        <Note
-          note={{
-            text: "sad",
-            time: "1:00PM",
-            color: "cyan",
-          }}
-        />
+        {props.notes.map((item, index) => (
+          <Note key={index} note={item} />
+        ))}
       </div>
     </div>
   );
