2277adb62c97156520c9b5bbd8b09b905b8c2043
sandeshstha1222 <sananddesh1029@gmail.com>
2024-01-08 12:25:57 +0545

    first commit

---
+++ src/components/NoteContainer/NoteContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./NoteContainer.css";
+import Note from "../Note/Note";
+
+const NoteContainer = () => {
+  return (
+    <div>
+      <Note />
+    </div>
+  );
+};
+
+export default NoteContainer;
